587d2f4ebb867abff03279af8c519be469309221
--get <--get>
2026-01-09 17:42:01 -0500

    Upload project to GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/fundacion-entre-lineas/',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/styles/_variables.scss";`
+        }
+      }
+    }
+  }
+});
